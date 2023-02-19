8e45b3128c711daf97a5ba382e169eb0bb68868e
Lakshay Pruthi <lakshay47pruthi16@gmail.com>
2023-02-19 11:16:37 +0530

    Task page added

---
+++ Pages/Tasks/Data/TaskData.js
@@ -0,0 +1,87 @@
+const TaskData =
+[
+    {
+      taskNumber: 1,
+      taskName: "Design User Interface",
+      active: true,
+      openingDate: "2022-01-01",
+      expectedCompletionDate: "2022-01-15",
+      difficulty : "Hard"
+    },
+    {
+      taskNumber: 2,
+      taskName: "Develop Backend API",
+      active: true,
+      openingDate: "2022-01-16",
+      expectedCompletionDate: "2022-02-01",
+      difficulty : "Easy"
+    },
+    {
+      taskNumber: 3,
+      taskName: "Integrate Frontend and Backend",
+      active: false,
+      openingDate: "2022-02-02",
+      expectedCompletionDate: "2022-02-15",
+      difficulty : "Hard"
+    },
+    {
+      taskNumber: 4,
+      taskName: "Test and Debug Application",
+      active: false,
+      openingDate: "2022-02-16",
+      expectedCompletionDate: "2022-03-01",
+      difficulty : "Easy"
+    },
+    {
+      taskNumber: 5,
+      taskName: "Deploy Application",
+      active: false,
+      openingDate: "2022-03-02",
+      expectedCompletionDate: "2022-03-15",
+      difficulty : "Medium"
+    },
+    {
+      taskNumber: 6,
+      taskName: "Implement User Authentication",
+      active: false,
+      openingDate: "2022-03-16",
+      expectedCompletionDate: "2022-04-01",
+      difficulty : "Hard"
+    },
+    {
+      taskNumber: 7,
+      taskName: "Integrate Payment System",
+      active: false,
+      openingDate: "2022-04-02",
+      expectedCompletionDate: "2022-04-15",
+      difficulty : "Easy"
+    },
+    {
+      taskNumber: 8,
+      taskName: "Develop Mobile Application",
+      active: false,
+      openingDate: "2022-04-16",
+      expectedCompletionDate: "2022-05-01",
+      difficulty : "Medium"
+    },
+    {
+      taskNumber: 9,
+      taskName: "Implement User Feedback System",
+      active: false,
+      openingDate: "2022-05-02",
+      expectedCompletionDate: "2022-05-15",
+      difficulty : "Easy"
+    },
+    {
+      taskNumber: 10,
+      taskName: "Integrate Analytics and Reporting",
+      active: false,
+      openingDate: "2022-05-16",
+      expectedCompletionDate: "2022-06-01",
+      difficulty : "Hard"
+    }
+  ];
+  
+  
+
+    export default TaskData;
