749bd654dfd9c2ded94d2e8bb1850339e9c85c6f
Lakshay Pruthi <91469717+Lakshay-Pruthi@users.noreply.github.com>
2023-04-08 12:52:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import SignUp from "./Pages/SignUp/SignUp";
 import Tasks from "./Pages/Tasks/Tasks";
 import Profile from "./Pages/Profile/Profile";
 
-import { Routes, Route } from 'react-router-dom'
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
+    <BrowserRouter>
     <Routes>
-      <Route path="/" element={<SignUp />}></Route>
-      <Route path="/Dashboard" element={<Dashboard />}></Route>
-      <Route path="/Project" element={<Project />}></Route>
-      <Route path="/Tasks" element={<Tasks />}></Route>
-      <Route path="/Profile" element={<Profile />}></Route>
-      <Route path="/Chat" element={<Chat/>}></Route>
+      <Route path="/" element={<SignUp />} />
+      <Route path="/Dashboard" element={<Dashboard /> /}>
+      <Route path="/Project" element={<Project />} />
+      <Route path="/Tasks" element={<Tasks />} />
+      <Route path="/Profile" element={<Profile />} />
+      <Route path="/Chat" element={<Chat/>} />
     </Routes>
+</BrowserRouter>
   );
 }
 
